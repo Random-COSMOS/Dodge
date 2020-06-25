45c3e5033b3cb1a5961486a1932f7c0e7d4193df
RANDOM cosmos <srijanwhat2@gmail.com>
2020-06-25 23:56:24 +0530

    Fixed high score deletion system

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const dataStore = require('nedb');
-const bodyParser = require('body-parser')
-const multer = require('multer');
-const upload = multer()
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Listning at ${port}! \nhttp://localhost:3000`));
+app.listen(port, () => console.log(`Listening at ${port}! \nhttp://localhost:3000`));
 app.use(express.static('public'));
 app.use(express.json({
     limit: "1mb"
 }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(upload.array());
-
 
 const db = new dataStore({
     filename: 'High Scores.db',
@@ -32,29 +23,36 @@ app.post("/checkScores", (req, res) => {
             $gt: score
         }
     }, (err, doc) => {
-        doc.length != 10 ? highScore = true : highScore = false
+        doc.length < 10 ? highScore = true : highScore = false
         res.json({
             status: "ok",
             score: score,
-            highScore: highScore
+            highScore: highScore,
         })
     })
 })
 
-app.post("/test", (req, res) => {
-    console.log(req.body)
-    res.send('okay')
+app.post("/highScore", (req, res) => {
+    data = req.body;
+    console.log('Its an High Score')
+
+    db.insert(data, (err, doc) => {
+        err ? console.log(err) : console.log(doc)
+    })
+
+    db.find({}).sort({
+        score: 1
+    }).exec((err, doc) => {
+        console.log(doc)
+        console.log(doc.length)
+
+        if (doc.length > 10) {
+            id = doc[0]._id
+            db.remove({
+                _id: id
+            }, (err, numRemoved) => {
+                err ? console.log(err) : null
+            })
+        }
+    })
 })
-
-// app.post("/highScore", (req, res) => {
-//     data = req.body;
-//     console.log('Its an High Score')
-
-//     database.insert(data, (err, doc) => {
-//         if (err) {
-//             console.log("error: " + err)
-//         } else {
-//             console.log(`Inserted ${doc.name} with ID ${doc._id}`);
-//         }
-//     })
-// })
